b34cce4c47dd074c8518d92879048d8bbe7d9d4f
johannes <johannes@kaesereibe.fritz.box>
2018-11-03 15:46:09 +0100

    Deserializer done

--- src/main/java/de/hskl/cs/master/JSON/PersonalDeserializer.java
+++ src/main/java/de/hskl/cs/master/JSON/PersonalDeserializer.java
@@ -2,7 +2,10 @@ package de.hskl.cs.master.JSON;
 
 import java.lang.reflect.Type;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -16,18 +19,34 @@ public class PersonalDeserializer  implements JsonDeserializer<Actor> {
 	public Actor deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
 			throws JsonParseException {
 		
+		
 		 JsonObject jsonObj = json.getAsJsonObject();
+		 GsonBuilder gsonBuilder = new GsonBuilder();
+		 Gson gson = gsonBuilder
+					.setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+					.create();
+		 
+		 
 		 String name = jsonObj.get("name").getAsString(); 
 		 String gender = jsonObj.get("gender").getAsString(); 
-		 String homeworld = jsonObj.get("homeworld").getAsString();
-		 JsonElement ships = jsonObj.getAsJsonObject().get("spaceships");
-		 JsonElement films = jsonObj.getAsJsonObject("films");
-		 //String telefonNr = jsonObj.get("TelefonNr").getAsString();
-	Actor actor = new Actor(name, gender, new Planet(homeworld));
-			actor.addStarship(new Gson().fromJson(ships, Starship.class));
-			actor.addFilm(new Gson().fromJson(films, Film.class));
-	return actor; }
+		 Planet homeworld =  gson.fromJson(jsonObj.get("Homeworld"), Planet.class);
+		 
+		 JsonArray ships = jsonObj.getAsJsonArray("spaceships");
+		 JsonArray films = jsonObj.getAsJsonArray("films");
+		
+		 Actor actor = new Actor(name, gender, homeworld);
+			
+		 for(JsonElement film : films) {
+			 actor.addFilm(gson.fromJson(film, Film.class));
+		 }
+		 
+		 for(JsonElement ship : ships) {
+			 actor.addStarship(gson.fromJson(ship, Starship.class));
+		 }	
+		 return actor;
+	}
 
+	
 }
 
 	
